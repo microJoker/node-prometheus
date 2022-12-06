6f6cd339bcddcc96faef3c421fa6beeb04747904
EaSonChan <yixin.chen@tenclass.com>
2022-12-06 14:52:02 +0800

    init node-prometheus

--- examples/example.js
+++ examples/example.js
@@ -1,21 +1,7 @@
-# node-prometheus
- - 此项目为上报打点数据到prometheus
-
-## nodejs项目接入普罗米修斯
-
-环境变量中必须要有以下环境变量
- - PROJECT_NAME
- - POD_IP
- - HOSTNAME
- - NAMESPACE
-
-example
-
-```js
 const {initSuperconf, CONFIG} = require('node-superconf')
 var express = require('express')
-const {PrometheusClient} = require('../prometheus/client')
-
+const {PrometheusClient} = require('node-prometheus/prometheus/client')
+    
 
 // 普罗米修斯客户端放中间件打点
 const prometheus_middleware = async (ctx, next) => {
@@ -27,10 +13,10 @@ const prometheus_middleware = async (ctx, next) => {
 }
 
 
-initSuperconf().then(() => {
+initSuperconf('xxx').then(() => {
     console.log('initSuperconf success => CONFIG: ', CONFIG);
 
-    const {prometheus_server, register_prometheus_metrics} = require('../prometheus/server')
+    const {prometheus_server, register_prometheus_metrics} = require('node-prometheus')
 
     // 本地起普罗米修斯服务，用于给架构组拉取打点数据
     prometheus_server.listen(28080, () => {
@@ -48,5 +34,3 @@ initSuperconf().then(() => {
         console.log('Listen at port: ' + 8080, new Date().getTime());
     })
 })
-
-```
